bc52485573895bc694dd3252e4a5e5c022cf0d45
kafedzie <2414012@leedstrinity.ac.uk>
2026-02-24 19:26:46 +0000

    Day 2 Challenge Complete

---
+++ day_2/variables.py
@@ -0,0 +1,70 @@
+# Day 2: 30DaysofPython
+
+first_name = 'Nana'
+last_name = 'Kodwo'
+full_name = 'Nana Kodwo'
+country = 'Ghana'
+city = 'Accra'
+age = 26
+year = 2001
+is_married = True
+is_true = False
+is_light_on = True
+
+f_name, l_name, n_age, n_is_married, n_country = 'Adjeiwa', 'Affram', '32', True, 'Ghana'
+
+print(f_name, l_name, n_age, n_is_married, n_country)
+print('First name: ', f_name)
+print('Last name: ', l_name)
+print('Age: ', n_age)
+print('Married: ', n_is_married)
+print('Country: ', n_country)
+
+#Data Types
+print(type(f_name))
+print(type(l_name))
+print(type(n_age))
+print(type(n_is_married))
+print(type(n_country))
+
+print('First name: ', first_name)
+print('First name length: ', len(first_name))
+print('Last name: ', len(last_name))
+
+
+# Arithmetic
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+
+diff = num_one - num_two
+
+product = num_two * num_one
+
+division = num_one / num_two
+
+remainder = num_one % num_two
+
+exp = num_one ** num_two
+
+floor_division = num_one // num_two
+
+# Area of a circle
+
+r = 30
+
+area_of_circle = (22/7) * (r ** 2)
+print(area_of_circle)
+
+circum_of_circle = 2 * (22/7) * r
+print(circum_of_circle)
+
+radius = input('Radius: ')
+
+area_of_circle_2 = (22/7) * (int(radius) ** 2)
+
+print(area_of_circle_2)
+
+
