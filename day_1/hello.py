9c3340c7b17767ff178ae6028c02dd96d8a9872c
kafedzie <2414012@leedstrinity.ac.uk>
2026-02-24 10:15:26 +0000

    Initial commit

---
+++ day_1/hello.py
@@ -0,0 +1,34 @@
+print(3 + 4)
+print(4 - 3)
+print(3 * 4)
+print(4 % 3)
+print(4/3)
+print(3 ** 4)
+print(4 // 3)
+
+print('Nana Kodwo')
+print('Afedzie')
+print('Ghana')
+print('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type((['Asabeneh'], 'Python', 'Finland')))
+print(type('Nana Kodwo'))
+print(type('Ghana'))
+
+# Finding Euclidean Distance (2, 3) and (10, 8)
+
+a = 2
+b = 3
+c = 10
+d = 8
+
+x = c - a
+y = d - b
+
+e_distance = ((x ** 2) + (y ** 2)) ** 0.5
+print(e_distance)
+
---
+++ hello.py
@@ -0,0 +1,25 @@
+# Day 1 - 30DaysofPython Challenge
+
+print(2 + 3)
+print(3 - 1)
+print(2 * 3)
+print(2/3)
+print(2**5)
+print(3%2)
+print(3//2)
+
+# Checking data types
+print(type(10))
+print(type(3.14))
+print(type(1 + 3j))
+print(type('Nana'))
+print([1, 2, 3])
+print(type({'name': 'Nana'}))
+print(type({9.8, 3.14, 2.7}))
+print(type((9.8, 3.14, 2.7)))
+
+print('Nana Kodwo')
+print('Afedzie')
+print('Ghana')
+print('I am enjoying 30 days of Python')
+
